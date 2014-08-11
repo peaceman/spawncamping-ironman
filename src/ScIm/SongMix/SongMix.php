323aab82dafa6361b103fd9223a61182fd115b31
peaceman <nixalio@gmail.com>
2014-08-11 11:20:05 +0200

    songmix model

---
+++ src/ScIm/SongMix/SongMix.php
@@ -0,0 +1,22 @@
+<?php
+namespace ScIm\SongMix;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+/**
+ * Class SongMix
+ * @package ScIm\SongMix
+ *
+ * @property int $id
+ * @property string $title
+ * @property int $length_in_seconds
+ * @property string $filename
+ *
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class SongMix extends Eloquent
+{
+	protected $table = 'song_mixes';
+}
