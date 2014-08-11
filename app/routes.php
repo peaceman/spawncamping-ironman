f656cef46b74034ae95f1ddd2ee78505564009a8
peaceman <nixalio@gmail.com>
2014-08-12 00:17:14 +0200

    admin routes and navigation

--- app/routes.php
+++ app/routes.php
@@ -20,6 +20,39 @@ Route::group(
 	['namespace' => 'ScIm\Controllers\Admin', 'prefix' => 'admin'],
 	function() {
 		Route::get('', ['as' => 'admin.home', 'uses' => 'PagesController@home']);
+
+		Route::group(
+			['prefix' => 'artists'],
+			function() {
+				Route::get('', ['as' => 'admin.artists.index', 'uses' => 'ArtistsController@index']);
+				Route::get('create', ['as' => 'admin.artists.create', 'uses' => 'ArtistsController@create']);
+				Route::post('', ['as' => 'admin.artists.store', 'uses' => 'ArtistsController@store']);
+				Route::get('{artistId}/edit', ['as' => 'admin.artists.edit', 'uses' => 'ArtistsController@edit']);
+				Route::put('{artistId}', ['as' => 'admin.artists.update', 'uses' => 'ArtistsController@update']);
+			}
+		);
+
+		Route::group(
+			['prefix' => 'genres'],
+			function() {
+				Route::get('', ['as' => 'admin.genres.index', 'uses' => 'GenresController@index']);
+				Route::get('create', ['as' => 'admin.genres.create', 'uses' => 'GenresController@create']);
+			}
+		);
+
+		Route::group(
+			['prefix' => 'record-labels'],
+			function() {
+				Route::get('', ['as' => 'admin.record-labels.index', 'uses' => 'RecordLabelsController@inex']);
+			}
+		);
+
+		Route::group(
+			['prefix' => 'songs'],
+			function() {
+				Route::get('', ['as' => 'admin.songs.index', 'uses' => 'SongsController@index']);
+			}
+		);
 	}
 );
 
--- app/views/layouts/partials/admin/nav.blade.php
+++ app/views/layouts/partials/admin/nav.blade.php
@@ -1,8 +1,22 @@
 <ul class="nav nav-pills nav-stacked">
-  <li class="active">
-    <a href="#">
-      <span class="badge pull-right">42</span>
-      Home
-    </a>
-  </li>
+    <li class="{{ setActive('admin.artists.index') }}">
+        <a href="{{{ route('admin.artists.index') }}}">
+            @lang('common.artists')
+        </a>
+    </li>
+    <li class="{{ setActive('admin.genres.index') }}">
+        <a href="{{{ route('admin.genres.index') }}}">
+            @lang('common.genres')
+        </a>
+    </li>
+    <li class="{{ setActive('admin.record-labels.index') }}">
+        <a href="{{{ route('admin.record-labels.index') }}}">
+            @lang('common.record-labels')
+        </a>
+    </li>
+    <li class="{{ setActive('admin.songs.index') }}">
+        <a href="{{{ route('admin.songs.index') }}}">
+            @lang('common.songs')
+        </a>
+    </li>
 </ul>
