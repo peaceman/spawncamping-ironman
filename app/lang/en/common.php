d6bbc4a09fc2babc6160f1579f5a32af2fbc14da
peaceman <nixalio@gmail.com>
2014-08-12 01:24:12 +0200

    translation

--- app/lang/en/common.php
+++ app/lang/en/common.php
@@ -1,10 +1,29 @@
 <?php
 /*************************************************************************
- Generated via "php artisan localization:missing" at 2014/08/11 12:03:51 
+ Generated via "php artisan localization:missing" at 2014/08/11 23:23:28 
 *************************************************************************/
 
 return array (
   //============================== New strings to translate ==============================//
+  'artist' => 'artist',
+  'artists' => 'artists',
+  'edit' => 'edit',
+  'genre' => 'genre',
+  'genres' => 'genres',
+  'id' => 'id',
+  'index' => 'index',
+  'logo_filename' => 'logo_filename',
+  'name' => 'name',
+  'no-entries' => 'no-entries',
+  'promotion-end' => 'promotion-end',
+  'promotion-start' => 'promotion-start',
+  'record-label' => 'record-label',
+  'record-labels' => 'record-labels',
+  'song' => 'song',
+  'songs' => 'songs',
+  'title' => 'title',
+  'updated_at' => 'updated_at',
+  //==================================== Translations ====================================//
   'email' => 'email',
   'login' => 'login',
   'logout' => 'logout',
